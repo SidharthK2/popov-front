a602b8c0ba763f718c55f3735d64d90e09183366
sidharthK2 <sidharthkapoor.100@gmail.com>
2023-07-22 11:12:07 +0100

    add web2 login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,155 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+import {
+  ADAPTER_EVENTS,
+  CHAIN_NAMESPACES,
+  SafeEventEmitterProvider,
+  UserInfo,
+  WALLET_ADAPTERS,
+} from "@web3auth/base";
+import { Box, Divider, Grid, Typography } from "@mui/material";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import { Web3AuthOptions } from "@web3auth/modal";
+import { EthHashInfo } from "@safe-global/safe-react-components";
+import {
+  AuthKitSignInData,
+  Web3AuthEventListener,
+  Web3AuthModalPack,
+  Web3AuthConfig,
+} from "@safe-global/auth-kit";
+
+const connectedHandler: Web3AuthEventListener = (data) =>
+  console.log("CONNECTED", data);
+const disconnectedHandler: Web3AuthEventListener = (data) =>
+  console.log("DISCONNECTED", data);
 
 export default function Home() {
+  const [web3AuthModalPack, setWeb3AuthModalPack] =
+    useState<Web3AuthModalPack>();
+  const [safeAuthSignInResponse, setSafeAuthSignInResponse] =
+    useState<AuthKitSignInData | null>(null);
+
+  const [userInfo, setUserInfo] = useState<Partial<UserInfo>>();
+  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
+    null
+  );
+
+  useEffect(() => {
+    (async () => {
+      const options: Web3AuthOptions = {
+        //move to env
+        clientId:
+          "BB7C9TBAnfZHiUviCvgQinjJoPARq46Bd-kjnvOIMe1rChb4KEbtzzZDNZKIcHWD3wfpgsA2wsUYUtvAtzoBUh0-dq_paOgQp5L_jswemrJRn8" ||
+          "",
+        web3AuthNetwork: "testnet",
+        chainConfig: {
+          chainNamespace: CHAIN_NAMESPACES.EIP155,
+          chainId: "0x5",
+          //move to env
+          rpcTarget: "https://rpc.ankr.com/eth_goerli",
+        },
+        uiConfig: {
+          theme: "dark",
+          loginMethodsOrder: ["google", "facebook"],
+        },
+      };
+
+      const modalConfig = {
+        [WALLET_ADAPTERS.TORUS_EVM]: {
+          label: "torus",
+          showOnModal: false,
+        },
+        [WALLET_ADAPTERS.METAMASK]: {
+          label: "metamask",
+          showOnDesktop: true,
+          showOnMobile: false,
+        },
+      };
+
+      const openloginAdapter = new OpenloginAdapter({
+        loginSettings: {
+          mfaLevel: "mandatory",
+        },
+        adapterSettings: {
+          uxMode: "popup",
+          whiteLabel: {
+            name: "Safe",
+          },
+        },
+      });
+
+      const web3AuthConfig: Web3AuthConfig = {
+        txServiceUrl: "https://safe-transaction-goerli.safe.global",
+      };
+
+      const web3AuthModalPack = new Web3AuthModalPack(web3AuthConfig);
+
+      await web3AuthModalPack.init({
+        options,
+        adapters: [],
+        modalConfig,
+      });
+
+      web3AuthModalPack.subscribe(ADAPTER_EVENTS.CONNECTED, connectedHandler);
+
+      web3AuthModalPack.subscribe(
+        ADAPTER_EVENTS.DISCONNECTED,
+        disconnectedHandler
+      );
+      console.log(web3AuthModalPack);
+      setWeb3AuthModalPack(web3AuthModalPack);
+
+      return () => {
+        web3AuthModalPack.unsubscribe(
+          ADAPTER_EVENTS.CONNECTED,
+          connectedHandler
+        );
+        web3AuthModalPack.unsubscribe(
+          ADAPTER_EVENTS.DISCONNECTED,
+          disconnectedHandler
+        );
+      };
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (web3AuthModalPack && web3AuthModalPack.getProvider()) {
+      console.log("logged in");
+      (async () => {
+        await login();
+      })();
+    }
+  }, [web3AuthModalPack]);
+
+  const login = async () => {
+    if (!web3AuthModalPack) return;
+
+    const signInInfo = await web3AuthModalPack.signIn();
+    console.log("SIGN IN RESPONSE: ", signInInfo);
+
+    const userInfo = await web3AuthModalPack.getUserInfo();
+    console.log("USER INFO: ", userInfo);
+
+    setSafeAuthSignInResponse(signInInfo);
+    setUserInfo(userInfo || undefined);
+    setProvider(web3AuthModalPack.getProvider() as SafeEventEmitterProvider);
+  };
+
+  const logout = async () => {
+    if (!web3AuthModalPack) return;
+
+    await web3AuthModalPack.signOut();
+
+    setProvider(null);
+    setSafeAuthSignInResponse(null);
+  };
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div>
+      <div>Home</div>
+      <button onClick={login}></button>
+      <div>{safeAuthSignInResponse?.eoa}</div>
+    </div>
+  );
 }
