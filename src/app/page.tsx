4928c27df5b33f5fee71add07f5864fd7b3964c9
Jeremy N <jkn1092@gmail.com>
2023-07-22 09:28:42 +0200

    add explore and detail dao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,63 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import {CardContent} from "@mui/material";
+
+const mock = [
+  {
+    title: 'Themeable',
+    infos:
+        'Customize any part of our components to match your design needs.',
+  },
+  {
+    title: 'Light and dark UI',
+    infos:
+        'Optimized for multiple color modes. Use light or dark, your choice.',
+  },
+  {
+    title: 'Composable',
+    infos:
+        'Designed with composition in mind. Compose new components with ease.',
+  },
+  {
+    title: 'Developer experience',
+    infos:
+        'Guaranteed to boost your productivity when building your app or website.',
+  },
+  {
+    title: 'Continuous updates',
+    infos: 'We continually deploy improvements and new updates to theFront.',
+  },
+  {
+    title: 'Free support',
+    infos:
+        '6 months of free technical support to help you build your website faster.',
+  },
+];
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          Get started with popov
         </p>
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            At{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/ethglobal.svg"
+              alt="EthGlobal Logo"
               className={styles.vercelLogo}
               width={100}
               height={24}
@@ -29,66 +68,118 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <Box>
+          <Box marginBottom={4}>
+            <Typography
+                variant={'h4'}
+                gutterBottom
+                align={'center'}
+                sx={{ fontWeight: 700 }}
+            >
+              The powerful and flexible theme
+              <br />
+              for all kinds of businesses
+            </Typography>
+            <Typography
+                variant={'h6'}
+                component={'p'}
+                align={'center'}
+            >
+              Build a beautiful, modern website with flexible, fully customizable,
+              atomic MUI components.
+              <br />
+              An experience you'd expect from a design system.
+            </Typography>
+            <Box marginTop={3} display={'flex'} justifyContent={'center'}>
+              <Button
+                  component={'a'}
+                  href={'https://mui.com/store/items/the-front-landing-page/'}
+                  target={'_blank'}
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  endIcon={
+                    <svg
+                        width={16}
+                        height={16}
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                      <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M17 8l4 4m0 0l-4 4m4-4H3"
+                      />
+                    </svg>
+                  }
+              >
+                Create now
+              </Button>
+            </Box>
+          </Box>
+          <Box maxWidth={800} margin={'0 auto'}>
+            <Grid container spacing={2}>
+              {mock.map((item, i) => (
+                  <Grid item xs={12} key={i}>
+                    <Box
+                        component={Card}
+                        variant={'outlined'}
+                        bgcolor={'transparent'}
+                        sx={{
+                          '&:hover': {
+                            boxShadow: 2,
+                          },
+                        }}
+                    >
+                      <Box
+                          component={CardContent}
+                          display={'flex'}
+                          alignItems={'center'}
+                      >
+                        <Box
+                            display={'flex'}
+                            flexDirection={{ xs: 'column', sm: 'row' }}
+                            flex={'1 1 100%'}
+                            justifyContent={{ sm: 'space-between' }}
+                            alignItems={{ sm: 'center' }}
+                        >
+                          <Typography
+                              variant={'h6'}
+                              fontWeight={700}
+                              sx={{ marginBottom: { xs: 1, sm: 0 } }}
+                          >
+                            {item.title}
+                          </Typography>
+                          <Typography variant={'subtitle1'} color={'text.secondary'}>
+                            {`${item.infos}`}
+                          </Typography>
+                        </Box>
+                        <Box marginLeft={2} color={'primary.main'}>
+                          <Box
+                              component={'svg'}
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                              width={{ xs: 30, sm: 40 }}
+                              height={{ xs: 30, sm: 40 }}
+                          >
+                            <path
+                                fillRule="evenodd"
+                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                clipRule="evenodd"
+                            />
+                          </Box>
+                        </Box>
+                      </Box>
+                    </Box>
+                  </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Box>
       </div>
     </main>
   )
